69b93748db946d883df4b9f9db538a556906343c
Gabee <116602883+GabeePaez@users.noreply.github.com>
2023-08-26 00:49:55 -0300

    Páginas añadidas
    .

---
+++ index.js
@@ -0,0 +1,8 @@
+let miNombre = "Gabriel";
+let miApellido = "Paez";
+let miEdad = 18;
+let miMascota = "Roco";
+let edadMascota = "nose";
+
+let nombreCompleto = ['Gabriel' + " " + 'Paez']
+console.log(nombreCompleto);
